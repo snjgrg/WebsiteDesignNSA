028d18a7ffe5b0c8567d529ac5f8ce4a428b211a
Sanjay <snjgrg1000@gmail.com>
2019-10-02 11:10:50 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WebsiteDesignNSA
+# Project Tandav
+
+We are making a multipurpose web app for Nepalese club at the The City College of New York.
+
+# GitHub:
+Basic workflow of our project:
+![](https://slack-files.com/TNX04UK46-FNZ1R3EK0-de46f68a3c)
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import {Layout,
+        Header, Navigation, Drawer,
+        Content, 
+        Footer, FooterSection,FooterLinkList,FooterDropDownSection} from 'react-mdl';
+
+
+import Main from './Components/main';
+import {Link} from 'react-router-dom';
+
+function App() {
+  return (
+  <div className="demo-big-content">
+      <Layout>
+        <Header className="header-color" >
+
+
+       
+            <Navigation className="header-color">
+            
+                <Link   to ="/home"><p className="name"><i class="fa fa-home" aria-hidden="true"/> home</p> </Link >
+                <Link  to ="/about_us"><p className="name"><i class="fa fa-id-badge" aria-hidden="true"/> about us </p></Link >
+                <Link  to ="/projects"><p className="name"><i class="fa fa-database" aria-hidden="true"/> projects </p></Link >
+                <Link  to ="/resources"><p className="name"><i class="fa fa-folder" aria-hidden="true"/> resources </p></Link >
+                <Link  to ="/contact"><p className="name"><i class="fa fa-envelope" aria-hidden="true"/> sign up </p></Link >
+                
+
+               
+                
+            </Navigation>
+       
+            
+            
+        </Header>
+            
+        <Content> 
+            <div className="page-content" />
+            <Main/>
+        </Content>
+        
+        <Footer className="footer-color">
+            
+            <p className="footer-notes">Copyright © 2019 NSA CCNY All Rights Reserved.
+                    
+            </p>  
+          
+        </Footer>
+
+      </Layout>
+    </div>
+        );
+}
+
+export default App;
